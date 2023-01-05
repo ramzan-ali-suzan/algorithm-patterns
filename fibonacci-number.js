2a37740dbd0fb25e6b43e8e20dca8b9eacf32a0a
Muhammad Ramzan Ali Suzan <ramzan.ali.suzan@gmail.com>
2023-01-05 20:29:35 +0600

    Solved fibonacci number

---
+++ fibonacci-number.js
@@ -0,0 +1,24 @@
+/**
+    Fibonacci Number
+    The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+    F(0) = 0, F(1) = 1
+    F(n) = F(n - 1) + F(n - 2), for n > 1.
+    Given n, calculate F(n).
+ */
+
+/**
+ * @param {number} n
+ * @returns {number}
+ */
+const fib = function (n) {
+    if (n < 0 || n > 30) {
+        throw new Error(`Input ${n} is out of range`);
+    }
+
+    if (n > 2) return fib(n - 1) + fib(n - 2);
+
+    return 1;
+}
+
+console.log(fib(30));
